12b5fb25aa20ba50d072e1301626dae0df0fa745
Craig Cavalier <craigcav@gmail.com>
2017-10-24 20:55:28 -0400

    add client-side graphql query

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { gql, graphql } from 'react-apollo';
+import PropTypes from 'prop-types';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  static propTypes = {
+    data: PropTypes.shape({
+      user: PropTypes.object
+    }).isRequired
+  };
   render() {
+    const { user } = this.props.data;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React: {user && user.firstName}</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +26,13 @@ class App extends Component {
   }
 }
 
-export default App;
+const query = gql`
+  query CurrentUser($id: String!) {
+    user(id: $id) {
+      id
+      firstName
+    }
+  }
+`;
+
+export default graphql(query, { options: { variables: { id: '40' } } })(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
+const client = new ApolloClient({
+  networkInterface,
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
